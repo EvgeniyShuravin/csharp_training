84afed3d15017b4f5663518ec667e47359fcee9e
EvgeniyShuravin <eugeniy.shuravin@gmail.com>
2023-01-26 17:20:06 +0300

    №18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Tests/GroupCreationTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData group = new GroupData()
+            {
+                Name = "testl"
+            };
+
+            app.Groups.Add(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(group);
+            newGroups.Sort();
+            oldGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
